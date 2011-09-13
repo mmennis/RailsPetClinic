b0f9650ededdb05958e924fa4976ebf2106643a9
Michael Mennis <mmennis@gmail.com>
2011-09-13 10:09:06 -0700

    Initial commit - files for basic rails 3.0.10 setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsPetClinic::Application.config.secret_token = '7e6f85d1a0c403f9fa891dc8565c9a9ba88205b3d7d5ac4779b5f414a3abc061d7540e1f8228adb717ddd9ccf4de7d3a27e00b9fecafdbbea42cc9963c274acc'
