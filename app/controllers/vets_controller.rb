fd8766ea27930ccac59768517a32e9e9d1603072
Michael Mennis <mmennis@gmail.com>
2011-09-13 16:28:23 -0700

    Added a header and footer. Fixed owners and vets pages to do n+1 queries. Added some pictures for fun Added the petclinic stylesheet.

--- app/controllers/vets_controller.rb
+++ app/controllers/vets_controller.rb
@@ -14,7 +14,8 @@ class VetsController < ApplicationController
   # GET /vets/1.xml
   def show
     @vet = Vet.find(params[:id])
-
+    
+    @specialties_line = @vet.specialties.collect { |s| s.name }.join(", ")
     respond_to do |format|
       format.html # show.html.erb
       format.xml  { render :xml => @vet }
