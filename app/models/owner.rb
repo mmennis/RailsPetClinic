9d6fae03dc1d485feae4f7f561fc1525d2f10d58
Michael Mennis <mmennis@gmail.com>
2011-09-14 10:16:21 -0700

    Added find_by_last_name for filtering functionality. Added test code and fixtures to verify filtering.

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -2,4 +2,17 @@ class Owner < ActiveRecord::Base
   
   has_many :pets
   
+  validates :first_name, :presence => true
+  validates :last_name, :presence => true
+  validates :address, :presence => true
+  validates :city, :presence => true
+  validates :telephone, :presence => true
+  
+  def self.find_by_last_name(last_name)
+    if last_name
+      Owner.where("last_name like :last_name ", {:last_name => "%#{last_name}%" })
+    else
+      Owner.find(:all)
+    end
+  end
 end
--- test/unit/owner_test.rb
+++ test/unit/owner_test.rb
@@ -1,8 +1,68 @@
 require 'test_helper'
 
 class OwnerTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  
+  def setup
+    @attrs = {
+      :first_name => 'First',
+      :last_name => 'Last',
+      :address => '1234 Address Lane',
+      :city => 'NeverNeverLand',
+      :telephone => '123-456-7890'
+    }
   end
+  
+  test "should not save without a first_name" do
+    @attrs.delete :first_name
+    owner = Owner.new @attrs
+    assert !owner.save
+  end
+  
+  test "should not save without a last_name" do
+    @attrs.delete :last_name
+    owner = Owner.new @attrs
+    assert !owner.save
+  end
+  
+  test "should not save without an address" do
+    @attrs.delete :address
+    owner = Owner.new @attrs
+    assert !owner.save
+  end
+  
+  test "should not save without a city" do
+    @attrs.delete :city
+    owner = Owner.new @attrs
+    assert !owner.save
+  end
+  
+  test "should not save without a telephone" do
+    @attrs.delete :telephone
+    owner = Owner.new @attrs
+    assert !owner.save
+  end
+  
+  test "should find owners by last name" do
+    owners = Owner.find_by_last_name "Smith"
+    owners.each do |owner|
+      assert owner.last_name =~ /Smith/
+    end
+    assert owners.size == 3
+  end
+  
+  test "should return empty results if name not found" do
+    owners = Owner.find_by_last_name "Murphy"
+    assert owners.size == 0
+  end
+  
+  test "should return all owners if name is blank" do
+    owners = Owner.find_by_last_name ""
+    assert owners.size == 5
+  end
+  
+  test "should find all owners if search is empty or nil" do
+    owners = Owner.find_by_last_name nil
+    assert owners.size == 5
+  end
+  
 end
