1445bc68e39d5669b93a7a1524f35fb3845c6b37
Jade Rubick <jade@newrelic.com>
2011-09-22 21:09:39 +0000

    Update unicorn config

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,8 +1,10 @@
 app_path = "/web_apps/rails_pet_clinic"
 
-listen 3000
+working_directory app_path
+listen "/tmp/rails_pet_clinic.socket", :backlog => 64
+user 'www-data', 'www-data'
 
 worker_processes 3
-pid "#{app_path}/tmp/pids/unicorn.pid"
+pid "/tmp/unicorn.pid"
 stdout_path ="#{app_path}/log/unicorn.stdout.log"
 stderr_path ="#{app_path}/log/unicorn.stderr.log"
