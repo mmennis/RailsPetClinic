ced224dcd38b7cbb53723b49e4c5ec84ec164602
Jonathan Owens <jonathan@newrelic.com>
2012-03-22 10:58:33 -0700

    Add metadata.rb

---
+++ deploy/cookbooks/petclinic/metadata.rb
@@ -0,0 +1,8 @@
+maintainer       "Jonathan Owens"
+maintainer_email "jonathan@newrelic.com"
+license          "All Rights Reserved"
+description      "Load the sample pet clinic app"
+version          "0.1"
+depends           'mysql'
+depends           'nginx'
+depends           'monit'
